2188fffd53604ae86fcd2ec29f7b3c0ede1e6cdd
SreeDivyaSV <119918005+SreeDivyaSV@users.noreply.github.com>
2022-12-26 01:33:12 +0530

    changed

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,22 +1,74 @@
 import json
 
-import pytest
+
+
 
 import unittest
 
-from hello_world import app
+
+
+
+def lambda_handler(event, context):
+
+    number1 = event['Number1']
+
+    number2 = event['Number2']
+
+    print(sum(number1,number2))
+
+    print(product(number1,number2))
+
+    print(difference(number1,number2))
+
+    print(quotient(number1,number2))
+
+    
+
+def sum(number1,number2):
+
+   return number1 + number2
+
+def product(number1,number2):
+
+    return number1 * number2
+
+def difference(number1,number2):
+
+    return abs(number1 - number2)
+
+def quotient(number1,number2):
+
+    return number1 / number2
+
+
+
+
+    
 
 class TestSum(unittest.TestCase):
 
+
+
+
     def test_add(self):
-        self.assertEqual(app.sum(2,2), 4, "Should be 4")
+
+        self.assertEqual(sum(2,2), 4)
+
     def test_product(self):
-        self.assertEqual(app.product(3, 2), 6, "Should be 6")
+
+        self.assertEqual(product(4, 2), 8)
+
     def test_sub(self):
-        self.assertEqual(app.difference(4, 1), 3, "Should be 3")
+
+        self.assertEqual(difference(2, 1), 1)
+
     def test_division(self):
-        self.assertEqual(app.quotient(4,2), 2, "Should be 2")
+
+        self.assertEqual(quotient(4,2), 2)
+
+
+
 
 if __name__ == '__main__':
-    unittest.main()
 
+    unittest.main()
